2eedea253f6d7d5b23bf21e58761b4d64bc2946c
longlevan9x <longvan1296@gmail.com>
2024-04-14 22:40:13 +0900

    add func

--- nodejs-sdk/index.js
+++ nodejs-sdk/index.js
@@ -11,8 +11,11 @@ import {
   DeleteObjectCommand,
   DeleteBucketCommand,
   paginateListObjectsV2,
-  ListBucketsCommand ,
+  ListBucketsCommand,
+  ListObjectsV2Command,
   GetObjectCommand,
+  DeleteObjectsCommand,
+  GetBucketAclCommand
 } from "@aws-sdk/client-s3";
 
 // A region and credentials can be declared explicitly. For example
@@ -24,9 +27,7 @@ const s3Client = new S3Client({
   credentials: fromEnv()
 });
 
-export async function main() {
-
-
+async function main() {
   const listBucketsCommand = new ListBucketsCommand({});
   // List all Amazon S3 bucket. The epoch timestamp is appended
   const resListAllBucket = await s3Client.send(listBucketsCommand);
@@ -45,14 +46,7 @@ export async function main() {
 
   console.log(resCreateBucket);
 
-  // // Put an object into an Amazon S3 bucket.
-  // await s3Client.send(
-  //   new PutObjectCommand({
-  //     Bucket: bucketName,
-  //     Key: "my-first-object.txt",
-  //     Body: "Hello JavaScript SDK!",
-  //   })
-  // );
+
 
   // // Read the object.
   // const { Body } = await s3Client.send(
@@ -96,7 +90,19 @@ export async function main() {
   // }
 }
 
-export const helloS3 = async () => {
+const putObject = async () => {
+  // // Put an object into an Amazon S3 bucket.
+  const response = await s3Client.send(
+    new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET,
+      Key: "my-first-object.txt",
+      Body: "Hello JavaScript SDK!",
+    })
+  );
+  console.log(response);
+};
+
+const helloS3 = async () => {
   const command = new ListBucketsCommand({});
 
   const { Buckets } = await s3Client.send(command);
@@ -105,11 +111,93 @@ export const helloS3 = async () => {
   return Buckets;
 };
 
+const listObjects = async () => {
+  const command = new ListObjectsV2Command({
+    Bucket: process.env.S3_BUCKET,
+    // The default and maximum number of keys returned is 1000. This limits it to
+    // one for demonstration purposes.
+    MaxKeys: 5,
+  });
+
+  const res = await s3Client.send(command);
+  console.log(res);
+}
+
+const deleteBucket = async () => {
+  const command = new DeleteBucketCommand({
+    Bucket: "test-bucket-1713099568769",
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteObject = async () => {
+  const command = new DeleteObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: "2024-04-14-12-12-33-6851129BD308AC69",
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteObjects = async () => {
+  const command = new DeleteObjectsCommand({
+    Bucket: process.env.S3_BUCKET,
+    Delete: {
+      Objects: [{ Key: "2024-04-14-12-21-18-96A087587FD77C6B" }, { Key: "object2.txt" }],
+    },
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    console.log(response);
+
+    if (response.Deleted) {
+      console.log(
+        `Successfully deleted ${response.Deleted.length} objects from S3 bucket. Deleted objects:`,
+      );
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+const getBucketAcl = async () => {
+  const command = new GetBucketAclCommand({
+    Bucket: process.env.S3_BUCKET,
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    const Grants = response.Grants;
+    console.log(Grants, response);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 // Call a function if this file was run directly. This allows the file
 // to be runnable without running on import.
 import { fileURLToPath } from "url";
 import { exit } from "process";
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   // main();
-  helloS3();
+  // helloS3();
+  // listObjects();
+  // deleteBucket();
+  // deleteObject();
+  // deleteObjects();
+  // getBucketAcl();
+  putObject();
 }
